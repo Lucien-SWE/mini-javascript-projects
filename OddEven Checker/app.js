f540b0e569df03854a9d31fa11f40da26efe56bd
Memtoz-dev <xiusmo4@gmail.com>
2025-11-14 19:19:13 +0000

    javascript main functionality implemented. no error handling yet

---
+++ OddEven Checker/app.js
@@ -0,0 +1,22 @@
+const inputBtn = document.getElementById("input-button");
+const outputVal = document.getElementById("output");
+
+
+inputBtn.addEventListener("click", function(){
+    let userInput = prompt("What number should I check?");
+    let answer = oddEven(userInput);
+
+    outputVal.innerText = answer;
+});
+
+function oddEven(x){
+    let check = x / 2;
+    let check2 = Number.isInteger(check);
+
+    if (check2 == true){
+        return "Even"
+    }
+    else {
+        return "Odd"
+    };
+};
